256c2ec177c09a39db0425de778c6f5c60086e40
zhengsizuo <42198964+zhengsizuo@users.noreply.github.com>
2020-03-30 09:59:08 +0800

    Add files via upload

---
+++ 动态规划/198-打家劫舍.py
@@ -0,0 +1,57 @@
+"""递归法，自顶向下，超出时间限制"""
+# class Solution(object):
+#     def rob(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         if not nums:
+#             return 0
+#         # 边界条件
+#         if len(nums) == 1 or len(nums)==2:
+#             return max(nums)
+#         if len(nums) == 3:
+#             return max(nums[0]+nums[2], nums[1])
+        
+#         if len(nums) >= 4:
+#             return max(nums[0]+self.rob(nums[2:]), nums[1]+self.rob(nums[3:]))
+
+"""动态规划，自底向上"""
+# class Solution(object):
+#     def rob(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         if not nums:
+#             return 0
+        
+#         # 边界条件
+#         if len(nums)==1 or len(nums)==2:
+#             return max(nums)
+
+#         dp = [nums[0], max(nums[:2])]
+#         for i in range(2, len(nums)):
+#             dp.append(max(dp[i-1], dp[i-2]+nums[i]))
+        
+#         return dp[-1]
+
+"""进阶，空间优化为O(1)"""
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        prev, curr = 0, 0
+        for i in range(len(nums)):
+            # curr: dp[k-1]; prev: dp[k-2]
+            temp = max(curr, prev+nums[i])
+            prev = curr  # dp[k-1]
+            curr = temp  # dp[k]
+        
+        return curr
+# nums = [1, 2, 3, 1]
+nums = [2, 10, 9, 1, 3]
+sl = Solution()
+print(sl.rob(nums))
